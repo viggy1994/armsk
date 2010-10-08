fd9e07b50c5676722568fdbe2e2a06137d431c68
yinfaichan <yinfaichan@d139828e-27a6-a81a-1e35-1eec2581caf8>
2010-10-08 08:23:58 +0000

    Camera Stream

---
+++ trunk/Camera Stream/build.sh
@@ -0,0 +1,1 @@
+make OPENCV_ROOT=../../../android-opencv/opencv/ V=0
---
+++ trunk/Camera Stream/obj/local/armeabi-v7a/objs/armskdemoapp/gen/armskdemoapp_swig.o.d
@@ -0,0 +1,140 @@
+/Users/yinfaichan/justus/ARmskDemoApp/ARmskDemoApp/obj/local/armeabi-v7a/objs/armskdemoapp/gen/armskdemoapp_swig.o: \
+ /Users/yinfaichan/justus/ARmskDemoApp/ARmskDemoApp/jni/gen/armskdemoapp_swig.cpp \
+ /Users/yinfaichan/android-ndk-r4-crystax/build/platforms/android-5/arch-arm/usr/include/jni.h \
+ /Users/yinfaichan/android-ndk-r4-crystax/build/platforms/android-5/arch-arm/usr/include/stdlib.h \
+ /Users/yinfaichan/android-ndk-r4-crystax/build/platforms/android-5/arch-arm/usr/include/sys/cdefs.h \
+ /Users/yinfaichan/android-ndk-r4-crystax/build/platforms/android-5/arch-arm/usr/include/sys/cdefs_elf.h \
+ /Users/yinfaichan/android-ndk-r4-crystax/build/platforms/android-5/arch-arm/usr/include/string.h \
+ /Users/yinfaichan/android-ndk-r4-crystax/build/platforms/android-5/arch-arm/usr/include/malloc.h \
+ /Users/yinfaichan/android-ndk-r4-crystax/build/platforms/android-5/arch-arm/usr/include/alloca.h \
+ /Users/yinfaichan/android-ndk-r4-crystax/build/platforms/android-5/arch-arm/usr/include/strings.h \
+ /Users/yinfaichan/android-ndk-r4-crystax/build/platforms/android-5/arch-arm/usr/include/sys/types.h \
+ /Users/yinfaichan/android-ndk-r4-crystax/build/platforms/android-5/arch-arm/usr/include/stdint.h \
+ /Users/yinfaichan/android-ndk-r4-crystax/build/platforms/android-5/arch-arm/usr/include/sys/_types.h \
+ /Users/yinfaichan/android-ndk-r4-crystax/build/platforms/android-5/arch-arm/usr/include/machine/_types.h \
+ /Users/yinfaichan/android-ndk-r4-crystax/build/platforms/android-5/arch-arm/usr/include/linux/posix_types.h \
+ /Users/yinfaichan/android-ndk-r4-crystax/build/platforms/android-5/arch-arm/usr/include/linux/stddef.h \
+ /Users/yinfaichan/android-ndk-r4-crystax/build/platforms/android-5/arch-arm/usr/include/linux/compiler.h \
+ /Users/yinfaichan/android-ndk-r4-crystax/build/platforms/android-5/arch-arm/usr/include/asm/posix_types.h \
+ /Users/yinfaichan/android-ndk-r4-crystax/build/platforms/android-5/arch-arm/usr/include/asm/types.h \
+ /Users/yinfaichan/android-ndk-r4-crystax/build/platforms/android-5/arch-arm/usr/include/linux/types.h \
+ /Users/yinfaichan/android-ndk-r4-crystax/build/platforms/android-5/arch-arm/usr/include/machine/kernel.h \
+ /Users/yinfaichan/android-ndk-r4-crystax/build/platforms/android-5/arch-arm/usr/include/sys/sysmacros.h \
+ /Users/yinfaichan/android-ndk-r4-crystax/build/platforms/android-5/arch-arm/usr/include/memory.h \
+ /Users/yinfaichan/justus/ARmskDemoApp/ARmskDemoApp/jni/Processor.h \
+ ../../../android-opencv/opencv//modules/core/include/opencv2/core/core.hpp \
+ ../../../android-opencv/opencv//modules/core/include/opencv2/core/types_c.h \
+ /Users/yinfaichan/android-ndk-r4-crystax/build/platforms/android-5/arch-arm/usr/include/assert.h \
+ /Users/yinfaichan/android-ndk-r4-crystax/build/platforms/android-5/arch-arm/usr/include/math.h \
+ /Users/yinfaichan/android-ndk-r4-crystax/build/platforms/android-5/arch-arm/usr/include/limits.h \
+ /Users/yinfaichan/android-ndk-r4-crystax/build/platforms/android-5/arch-arm/usr/include/sys/limits.h \
+ /Users/yinfaichan/android-ndk-r4-crystax/build/platforms/android-5/arch-arm/usr/include/linux/limits.h \
+ /Users/yinfaichan/android-ndk-r4-crystax/build/platforms/android-5/arch-arm/usr/include/machine/internal_types.h \
+ /Users/yinfaichan/android-ndk-r4-crystax/build/platforms/android-5/arch-arm/usr/include/machine/limits.h \
+ ../../../android-opencv/opencv//modules/core/include/opencv2/core/version.hpp \
+ ../../../android-opencv/opencv//modules/core/include/opencv2/core/operations.hpp \
+ ../../../android-opencv/opencv//modules/core/include/opencv2/core/mat.hpp \
+ ../../../android-opencv/opencv//modules/core/include/opencv2/core/flann.hpp \
+ ../../../android-opencv/opencv//modules/features2d/include/opencv2/features2d/features2d.hpp \
+ ../../../android-opencv/opencv//modules/highgui/include/opencv2/highgui/highgui.hpp \
+ ../../../android-opencv/opencv//modules/highgui/include/opencv2/highgui/highgui_c.h \
+ ../../../android-opencv/opencv//modules/core/include/opencv2/core/core_c.h \
+ ../../../android-opencv/opencv//modules/imgproc/include/opencv2/imgproc/imgproc.hpp \
+ ../../../android-opencv/opencv//modules/imgproc/include/opencv2/imgproc/types_c.h \
+ ../../../android-opencv/opencv//modules/calib3d/include/opencv2/calib3d/calib3d.hpp \
+ ../../../android-opencv/opencv//modules/calib3d/include/opencv2/calib3d/compat_c.h \
+ ../../../android-opencv/opencv//modules/imgproc/include/opencv2/imgproc/imgproc_c.h \
+ ../../../android-opencv/opencv//modules/imgproc/include/opencv2/imgproc/compat_c.h \
+ ../../../android-opencv/opencv//android/jni/image_pool.h
+
+/Users/yinfaichan/android-ndk-r4-crystax/build/platforms/android-5/arch-arm/usr/include/jni.h:
+
+/Users/yinfaichan/android-ndk-r4-crystax/build/platforms/android-5/arch-arm/usr/include/stdlib.h:
+
+/Users/yinfaichan/android-ndk-r4-crystax/build/platforms/android-5/arch-arm/usr/include/sys/cdefs.h:
+
+/Users/yinfaichan/android-ndk-r4-crystax/build/platforms/android-5/arch-arm/usr/include/sys/cdefs_elf.h:
+
+/Users/yinfaichan/android-ndk-r4-crystax/build/platforms/android-5/arch-arm/usr/include/string.h:
+
+/Users/yinfaichan/android-ndk-r4-crystax/build/platforms/android-5/arch-arm/usr/include/malloc.h:
+
+/Users/yinfaichan/android-ndk-r4-crystax/build/platforms/android-5/arch-arm/usr/include/alloca.h:
+
+/Users/yinfaichan/android-ndk-r4-crystax/build/platforms/android-5/arch-arm/usr/include/strings.h:
+
+/Users/yinfaichan/android-ndk-r4-crystax/build/platforms/android-5/arch-arm/usr/include/sys/types.h:
+
+/Users/yinfaichan/android-ndk-r4-crystax/build/platforms/android-5/arch-arm/usr/include/stdint.h:
+
+/Users/yinfaichan/android-ndk-r4-crystax/build/platforms/android-5/arch-arm/usr/include/sys/_types.h:
+
+/Users/yinfaichan/android-ndk-r4-crystax/build/platforms/android-5/arch-arm/usr/include/machine/_types.h:
+
+/Users/yinfaichan/android-ndk-r4-crystax/build/platforms/android-5/arch-arm/usr/include/linux/posix_types.h:
+
+/Users/yinfaichan/android-ndk-r4-crystax/build/platforms/android-5/arch-arm/usr/include/linux/stddef.h:
+
+/Users/yinfaichan/android-ndk-r4-crystax/build/platforms/android-5/arch-arm/usr/include/linux/compiler.h:
+
+/Users/yinfaichan/android-ndk-r4-crystax/build/platforms/android-5/arch-arm/usr/include/asm/posix_types.h:
+
+/Users/yinfaichan/android-ndk-r4-crystax/build/platforms/android-5/arch-arm/usr/include/asm/types.h:
+
+/Users/yinfaichan/android-ndk-r4-crystax/build/platforms/android-5/arch-arm/usr/include/linux/types.h:
+
+/Users/yinfaichan/android-ndk-r4-crystax/build/platforms/android-5/arch-arm/usr/include/machine/kernel.h:
+
+/Users/yinfaichan/android-ndk-r4-crystax/build/platforms/android-5/arch-arm/usr/include/sys/sysmacros.h:
+
+/Users/yinfaichan/android-ndk-r4-crystax/build/platforms/android-5/arch-arm/usr/include/memory.h:
+
+/Users/yinfaichan/justus/ARmskDemoApp/ARmskDemoApp/jni/Processor.h:
+
+../../../android-opencv/opencv//modules/core/include/opencv2/core/core.hpp:
+
+../../../android-opencv/opencv//modules/core/include/opencv2/core/types_c.h:
+
+/Users/yinfaichan/android-ndk-r4-crystax/build/platforms/android-5/arch-arm/usr/include/assert.h:
+
+/Users/yinfaichan/android-ndk-r4-crystax/build/platforms/android-5/arch-arm/usr/include/math.h:
+
+/Users/yinfaichan/android-ndk-r4-crystax/build/platforms/android-5/arch-arm/usr/include/limits.h:
+
+/Users/yinfaichan/android-ndk-r4-crystax/build/platforms/android-5/arch-arm/usr/include/sys/limits.h:
+
+/Users/yinfaichan/android-ndk-r4-crystax/build/platforms/android-5/arch-arm/usr/include/linux/limits.h:
+
+/Users/yinfaichan/android-ndk-r4-crystax/build/platforms/android-5/arch-arm/usr/include/machine/internal_types.h:
+
+/Users/yinfaichan/android-ndk-r4-crystax/build/platforms/android-5/arch-arm/usr/include/machine/limits.h:
+
+../../../android-opencv/opencv//modules/core/include/opencv2/core/version.hpp:
+
+../../../android-opencv/opencv//modules/core/include/opencv2/core/operations.hpp:
+
+../../../android-opencv/opencv//modules/core/include/opencv2/core/mat.hpp:
+
+../../../android-opencv/opencv//modules/core/include/opencv2/core/flann.hpp:
+
+../../../android-opencv/opencv//modules/features2d/include/opencv2/features2d/features2d.hpp:
+
+../../../android-opencv/opencv//modules/highgui/include/opencv2/highgui/highgui.hpp:
+
+../../../android-opencv/opencv//modules/highgui/include/opencv2/highgui/highgui_c.h:
+
+../../../android-opencv/opencv//modules/core/include/opencv2/core/core_c.h:
+
+../../../android-opencv/opencv//modules/imgproc/include/opencv2/imgproc/imgproc.hpp:
+
+../../../android-opencv/opencv//modules/imgproc/include/opencv2/imgproc/types_c.h:
+
+../../../android-opencv/opencv//modules/calib3d/include/opencv2/calib3d/calib3d.hpp:
+
+../../../android-opencv/opencv//modules/calib3d/include/opencv2/calib3d/compat_c.h:
+
+../../../android-opencv/opencv//modules/imgproc/include/opencv2/imgproc/imgproc_c.h:
+
+../../../android-opencv/opencv//modules/imgproc/include/opencv2/imgproc/compat_c.h:
+
+../../../android-opencv/opencv//android/jni/image_pool.h:
